2232f7b570a624b808070377f7f90111d2d94586
Christopher Lind <Christopher.lind.c@gmail.com>
2021-01-18 10:36:32 +0100

    Started Assignment. Made student class and added getter and setter. Concludes 10ø0 + 10ø1

--- Assignment10/Program.cs
+++ Assignment10/Program.cs
@@ -6,7 +6,28 @@ namespace Assignment10
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Solution10o1();
+        }
+
+        static void Solution10o0()
+        {
+            var student1 = new Student("Ben Solo");
+            var student2 = new Student("Han Solo");
+
+            Console.WriteLine($"{student1.Name} {student2.Name}");
+        }
+
+        static void Solution10o1()
+        {
+            var student1 = new Student("Ben Solo");
+            var student2 = new Student("Han Solo");
+
+            Console.WriteLine($"{student1.Name} {student2.Name}");
+
+            student1.SetValue("Jacen Solo");
+            student2.SetValue("Jaiden Solo");
+
+            Console.WriteLine($"{student1.Name} {student2.Name}");
         }
     }
 }
---
+++ Assignment10/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment10
+{
+    // Student class for 10ø0 and 10ø1
+    class Student
+    {
+        public string Name { get; private set; }
+
+        public Student(string name)
+        {
+            Name = name;
+        }
+
+        public void SetValue(string name) => Name = name;
+
+        public string GetValue() => Name;
+    }
+}
